44b4d389b806ec2a3b28dbcd25bb90fd900a24eb
Þorvaldur Rúnarsson <valdirunars@gmail.com>
2017-01-04 21:50:56 +0000

    Protection level SimpleJWT

--- Sources/SimpleSecure/SimpleJWT.swift
+++ Sources/SimpleSecure/SimpleJWT.swift
@@ -14,7 +14,7 @@ public enum MacAlgorithm {
     case hs256
     case hs384
     case hs512
-    
+
     public func toAlgorithm(with secret: Data) -> Algorithm {
         switch self {
         case .hs512:
@@ -29,22 +29,22 @@ public enum MacAlgorithm {
     }
 }
 public class SimpleJWT {
-    
+
     public static let tokenDuration: TimeInterval = 60 * 30 // 30 min
-    
+
     private let issuer: String
     public let scope: String
     private let algorithm: Algorithm
-    
+
     init(issuer: String, scope: String, using algorithm: MacAlgorithm, with secret: Data) {
         self.issuer = issuer
         self.scope = scope
         self.algorithm = algorithm.toAlgorithm(with: secret)
     }
-    
-    func encode(with secret: String, expiresIn: TimeInterval) -> String {
+
+    public func encode(with secret: String, expiresIn: TimeInterval) -> String {
         let token = JWT.encode(self.algorithm) { builder in
-            
+
             builder.issuer = issuer
             builder.issuedAt = Date()
             builder.expiration = Date().addingTimeInterval(expiresIn)
@@ -52,32 +52,32 @@ public class SimpleJWT {
         }
         return token
     }
-    
-    
-    func authorize(token: String) -> Bool {
+
+
+    public func authorize(token: String) -> Bool {
         do {
             let payload = try JWT.decode(token, algorithm: self.algorithm)
-            
+
             guard let iss = (payload["iss"] as? String) else {
                 return false
             }
-            
+
             guard let exp = (payload["exp"] as? TimeInterval) else {
                 return false
             }
-            
+
             guard let scp = (payload["scope"] as? String) else {
                 return false
             }
-            
+
             return Date().timeIntervalSince1970 < exp &&
                         iss == self.issuer &&
                         scp == self.scope
-            
+
         } catch {
             print("Failed to decode JWT: \(error)")
         }
         return false
     }
-    
+
 }
