06b1aa28b5f97824f59fc8f65505787b3f44ff4c
Þorvaldur Rúnarsson <valdirunars@gmail.com>
2017-01-04 21:39:28 +0000

    public Credential

--- Sources/SimpleSecure/SimpleCredential.swift
+++ Sources/SimpleSecure/SimpleCredential.swift
@@ -10,7 +10,11 @@ import Foundation
 import CryptoSwift
 
 public struct SimpleCredential {
-
+    public init(clientId: String, clientSecret: String, scope: String) {
+        self.clientId = clientId
+        self.clientSecret = clientSecret
+        self.scope = scope
+    }
     public let clientId: String
     public let clientSecret: String
     public let scope: String
