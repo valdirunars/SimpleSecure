687d870f6cfa103ff37b3f010a5d8c6594e48c2b
Þorvaldur Rúnarsson <valdirunars@gmail.com>
2017-01-06 02:15:56 +0000

    updated dependencies

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ let package = Package(
     name: "SimpleSecure",
     dependencies: [
         .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 2),
-        .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1, minor: 1),
+        .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 0, minor: 6),
         .Package(url: "https://github.com/IBM-Swift/SwiftyJSON.git", majorVersion: 15),
         .Package(url: "https://github.com/kylef/JSONWebToken.swift.git", majorVersion: 2, minor: 0)
     ]
