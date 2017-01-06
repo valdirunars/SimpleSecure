d12afc9fd737d3e881bd41e257caf75ae66ab170
Þorvaldur Rúnarsson <valdirunars@gmail.com>
2017-01-06 02:38:55 +0000

    updated kitura version

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "SimpleSecure",
     dependencies: [
-        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 2),
+        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 3),
         .Package(url: "https://github.com/IBM-Swift/SwiftyJSON.git", majorVersion: 15),
         .Package(url: "https://github.com/kylef/JSONWebToken.swift.git", majorVersion: 2, minor: 0)
     ]
